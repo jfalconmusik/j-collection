79e0c240c5985f130d903adeb7f6f5545fdd5cb9
jessecoyote <jfalconmusik@gmail.com>
2021-08-05 17:39:02 -0600

    changed environment vars, not using yaml

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router,
-Switch,
-Route,
-Link,
-useRouteMatch,
-useParams}
-from "react-router-dom";
-import './index.css';
-import { ContextProvider } from "./Context"
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+import "./index.css";
+import { ContextProvider } from "./Context";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// yml build commands:
+// - "# Execute Amplify CLI with the helper script"
+// - chmod u+x ./myamplifypush.sh
+// - ./myamplifypush.sh
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,7 +26,7 @@ ReactDOM.render(
       </Router>
     </ContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
